07d47a03bb48e135e8155d47703e8558ceb3d16d
nam-77 <137843628+nam-77@users.noreply.github.com>
2026-04-18 17:35:36 +0000

    updated fetch url

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,16 @@
-const btn = document.getElementById('gradeBtn');
+cconst btn = document.getElementById('gradeBtn');
 const input = document.getElementById('cardInput');
 const output = document.getElementById('output');
 
 btn.addEventListener('click', async () => {
-  const cardName = input.value;
+    const cardName = input.value;
 
-  const res = await fetch('http://localhost:3001/grade', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ cardName })
-  });
+    const res = await fetch("https://laughing-capybara-ggggv9gpj47hv6pw-3001.app.github.dev/grade", {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ cardName })
+    });
 
-  const data = await res.json();
-  output.textContent = JSON.stringify(data, null, 2);
+    const data = await res.json();
+    output.textContent = JSON.stringify(data, null, 2);
 });
